699c2d9137c840ce24ab79d4f4b04b46a336e88b
AahzBrut <partyomenko@gmail.com>
2020-01-06 16:34:50 +0300

    Add swagger dependencies into pom; Add stub configuration;
    Closes #19

---
+++ src/main/java/ru/aahzbrut/reciperestapi/config/SwaggerConfig.java
@@ -0,0 +1,9 @@
+package ru.aahzbrut.reciperestapi.config;
+
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@EnableSwagger2
+@Configuration
+public class SwaggerConfig {
+}
