39d507f4e4e0796fe30576afec90539e1a17def3
AahzBrut <partyomenko@gmail.com>
2020-01-08 15:41:30 +0300

    Initial commit for Categories controller;
    Add stub CategoryController;

---
+++ src/main/java/ru/aahzbrut/reciperestapi/controllers/CategoryController.java
@@ -0,0 +1,7 @@
+package ru.aahzbrut.reciperestapi.controllers;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController(value = "/api/v1/categories/")
+public class CategoryController {
+}
